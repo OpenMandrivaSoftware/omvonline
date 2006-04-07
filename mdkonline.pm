763b5840c25f2e4d78bf0a568d11521ddce8af84
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2006-04-07 16:04:31 +0000

    - bump copyright years - remove daouda's dead email - list myself
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/mdkonline/trunk@53806 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdkonline.pm
+++ mdkonline.pm
@@ -1,3 +1,26 @@
+################################################################################
+# Mandriva Online functions                                                    # 
+#                                                                              #
+# Copyright (C) 2004-2005 Mandrakesoft                                         #
+#               2005-2006 Mandriva                                             #
+#                                                                              #
+# Daouda Lo                                                                    #
+# Thierry Vignaud <tvignaud at mandriva dot com>                               #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License Version 2 as            #
+# published by the Free Software Foundation.                                   #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.   #
+################################################################################
+
 package mdkonline;
 
 use strict;
