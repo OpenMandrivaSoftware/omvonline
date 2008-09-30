dc03b28bafad918aa94b8f560bca312f39aed1ba
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-09-30 17:17:54 +0000

    dd support for downloading distro list as user for 2008.1
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/mdkonline/trunk@247177 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ mdkapplet_urpm.pm
@@ -0,0 +1,55 @@
+package mdkapplet_urpm;
+
+# taken from urpmi:
+
+use MDK::Common;
+use urpm::download;
+
+sub userdir_prefix {
+    my ($_urpm) = @_;
+    '/tmp/.urpmi-';
+}
+sub userdir {
+    my ($urpm) = @_;
+    $< or return;
+
+    my $dir = ($urpm->{urpmi_root} || '') . userdir_prefix($urpm) . $<;
+    mkdir $dir, 0755; # try to create it
+
+    -d $dir && ! -l $dir or $urpm->{fatal}(1, sprintf("fail to create directory %s", $dir));
+    -o $dir && -w $dir or $urpm->{fatal}(1, sprintf("invalid owner for directory %s", $dir));
+
+    mkdir "$dir/partial";
+    mkdir "$dir/rpms";
+
+    $dir;
+}
+sub ensure_valid_cachedir {
+    my ($urpm) = @_;
+    if (my $dir = userdir($urpm)) {
+	$urpm->{cachedir} = $dir;
+    }
+    -w "$urpm->{cachedir}/partial" or $urpm->{fatal}(1, "Can not download packages into %s", "$urpm->{cachedir}/partial");
+}
+sub valid_cachedir {
+    my ($urpm) = @_;
+    userdir($urpm) || $urpm->{cachedir};
+}
+
+sub get_content {
+    my ($urpm, $url) = @_;
+
+    my $download_dir = valid_cachedir($urpm) . '/partial/';
+    my $file = $download_dir . basename($url);
+
+    unlink $file; # prevent "partial file" errors
+    urpm::download::sync($urpm, undef, [ $url ], quiet => 1, dir => $download_dir) or return;
+    #sync_url($urpm, $url, dir => $download_dir, quiet => 1) or return;
+
+    my @l = cat_($file);
+    unlink $file;
+
+    wantarray() ? @l : join('', @l);
+}
+    
+1;
